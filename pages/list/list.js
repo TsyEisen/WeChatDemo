1251dae28ca6d19465c5a7b99ae5637692fe56b6
tsy <tsyios@163.com>
2016-12-02 17:01:19 +0800

    列表

---
+++ pages/list/list.js
@@ -0,0 +1,208 @@
+Page({
+  data:{
+      array: [
+        {
+          "sinaid": "200868",
+          "avgprice": "19036",
+          "priceunit": "元/平",
+          "communityname": "早安北京",
+          "picurl": "http://i1.esfimg.com/imp/542c93a33ac5b741b971570ca7e6471a_s200X150_os8ed3e7.jpg",
+          "defpic": "http://i1.esfimg.com/imp/542c93a33ac5b741b971570ca7e6471a_s200X150_os8ed3e7.jpg",
+          "propertytype": "普通住宅",
+          "deliverdate": "暂无",
+          "distance": "-1",
+          "station_name": "",
+          "district": "北京周边",
+          "block": "大厂",
+          "gouprate": "0",
+          "ratesign": "1",
+          "showtext": "北京周边-大厂",
+          "trade_count": "0"
+        },
+        {
+          "sinaid": "200980",
+          "avgprice": "57065",
+          "priceunit": "元/平",
+          "communityname": "领秀翡翠山",
+          "picurl": "http://pic2.esf.leju.com/picdata_bj/xq/2014/11/13/e7/d4/thum_679055246df28828e70eb53dfe611dae.jpg",
+          "defpic": "http://pic2.esf.leju.com/picdata_bj/xq/2014/11/13/e7/d4/thum_679055246df28828e70eb53dfe611dae.jpg",
+          "propertytype": "普通住宅",
+          "deliverdate": "暂无",
+          "distance": "-1",
+          "station_name": "",
+          "district": "丰台区",
+          "block": "卢沟桥",
+          "gouprate": "0",
+          "ratesign": "1",
+          "showtext": "丰台区-卢沟桥",
+          "trade_count": "1"
+        },
+        {
+          "sinaid": "368",
+          "avgprice": "85470",
+          "priceunit": "元/平",
+          "communityname": "珠江帝景",
+          "picurl": "http://pic2.esf.leju.com/picdata_bj/xq/2011/04/01/e7/d4/thum_8b96906795b8f014c9b7b711f8357044.jpg",
+          "defpic": "http://pic2.esf.leju.com/picdata_bj/xq/2011/04/01/e7/d4/thum_8b96906795b8f014c9b7b711f8357044.jpg",
+          "propertytype": "普通住宅",
+          "deliverdate": "2004年建造",
+          "distance": "-1",
+          "station_name": "",
+          "district": "朝阳区",
+          "block": "大望路",
+          "gouprate": "0.4",
+          "ratesign": "1",
+          "showtext": "朝阳区-大望路",
+          "trade_count": "18"
+        },
+        {
+          "sinaid": "338",
+          "avgprice": "81028",
+          "priceunit": "元/平",
+          "communityname": "芍药居北里",
+          "picurl": "http://i1.esfimg.com/imp/542c8ecf45458bba7d322493292f48ee_s200X150_osc68b4d.jpg",
+          "defpic": "http://i1.esfimg.com/imp/542c8ecf45458bba7d322493292f48ee_s200X150_osc68b4d.jpg",
+          "propertytype": "普通住宅",
+          "deliverdate": "1999年建造",
+          "distance": "-1",
+          "station_name": "",
+          "district": "朝阳区",
+          "block": "芍药居",
+          "gouprate": "0.3",
+          "ratesign": "1",
+          "showtext": "朝阳区-芍药居",
+          "trade_count": "52"
+        },
+        {
+          "sinaid": "1276",
+          "avgprice": "27947",
+          "priceunit": "元/平",
+          "communityname": "天洋城",
+          "picurl": "http://pic2.esf.leju.com/picdata_bj/xq/2011/02/22/e7/d4/thum_5e626f2733aee10597eedeb1a6bc27b7.jpg",
+          "defpic": "http://pic2.esf.leju.com/picdata_bj/xq/2011/02/22/e7/d4/thum_5e626f2733aee10597eedeb1a6bc27b7.jpg",
+          "propertytype": "普通住宅",
+          "deliverdate": "2011年建造",
+          "distance": "-1",
+          "station_name": "",
+          "district": "燕郊",
+          "block": "燕郊",
+          "gouprate": "0.3",
+          "ratesign": "-1",
+          "showtext": "燕郊-燕郊",
+          "trade_count": "95"
+        },
+        {
+          "sinaid": "210",
+          "avgprice": "69481",
+          "priceunit": "元/平",
+          "communityname": "远洋山水",
+          "picurl": "http://i3.esfimg.com/imp/542c9892e3c551d1ccb7cd4aeddbccd9_s200X150_os86e27e.jpg",
+          "defpic": "http://i3.esfimg.com/imp/542c9892e3c551d1ccb7cd4aeddbccd9_s200X150_os86e27e.jpg",
+          "propertytype": "普通住宅",
+          "deliverdate": "2005年建造",
+          "distance": "-1",
+          "station_name": "",
+          "district": "石景山",
+          "block": "鲁谷",
+          "gouprate": "1.5",
+          "ratesign": "1",
+          "showtext": "石景山-鲁谷",
+          "trade_count": "70"
+        },
+        {
+          "sinaid": "349",
+          "avgprice": "80714",
+          "priceunit": "元/平",
+          "communityname": "润枫水尚",
+          "picurl": "http://pic2.esf.leju.com/picdata_bj/xq/2010/09/27/16/c6/thum_9378f80d484f845e6795d49b9ced34e3.jpg",
+          "defpic": "http://pic2.esf.leju.com/picdata_bj/xq/2010/09/27/16/c6/thum_9378f80d484f845e6795d49b9ced34e3.jpg",
+          "propertytype": "公寓",
+          "deliverdate": "2009年建造",
+          "distance": "-1",
+          "station_name": "",
+          "district": "朝阳区",
+          "block": "朝青",
+          "gouprate": "3.4",
+          "ratesign": "1",
+          "showtext": "朝阳区-朝青",
+          "trade_count": "10"
+        },
+        {
+          "sinaid": "1384",
+          "avgprice": "64077",
+          "priceunit": "元/平",
+          "communityname": "沿海赛洛城",
+          "picurl": "http://i3.esfimg.com/imp/542c8f2dae059638a258b154fc210cf6_s200X150_osfd93fb.jpg",
+          "defpic": "http://i3.esfimg.com/imp/542c8f2dae059638a258b154fc210cf6_s200X150_osfd93fb.jpg",
+          "propertytype": "普通住宅",
+          "deliverdate": "2007年建造",
+          "distance": "-1",
+          "station_name": "",
+          "district": "朝阳区",
+          "block": "百子湾",
+          "gouprate": "0.9",
+          "ratesign": "1",
+          "showtext": "朝阳区-百子湾",
+          "trade_count": "24"
+        },
+        {
+          "sinaid": "6611",
+          "avgprice": "38719",
+          "priceunit": "元/平",
+          "communityname": "天通苑北一区",
+          "picurl": "http://pic2.esf.leju.com/exhibit1/build_pic_bd/6611.jpg",
+          "defpic": "http://pic2.esf.leju.com/exhibit1/build_pic_bd/6611.jpg",
+          "propertytype": "普通住宅",
+          "deliverdate": "2004年建造",
+          "distance": "-1",
+          "station_name": "",
+          "district": "昌平区",
+          "block": "天通苑",
+          "gouprate": "1.4",
+          "ratesign": "1",
+          "showtext": "昌平区-天通苑",
+          "trade_count": "37"
+        },
+        {
+          "sinaid": "9441",
+          "avgprice": "79335",
+          "priceunit": "元/平",
+          "communityname": "华贸城",
+          "picurl": "http://pic2.esf.leju.com/picdata_bj/xq/2012/11/02/e7/d4/thum_863ce59bb7ee4800066a9837dc0f6adc.jpg",
+          "defpic": "http://pic2.esf.leju.com/picdata_bj/xq/2012/11/02/e7/d4/thum_863ce59bb7ee4800066a9837dc0f6adc.jpg",
+          "propertytype": "公寓",
+          "deliverdate": "2010年建造",
+          "distance": "-1",
+          "station_name": "",
+          "district": "朝阳区",
+          "block": "北苑",
+          "gouprate": "3.2",
+          "ratesign": "1",
+          "showtext": "朝阳区-北苑",
+          "trade_count": "33"
+        }
+      ]
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+
+  },
+  onReady:function(){
+    // 页面渲染完成
+
+  },
+  onShow:function(){
+    // 页面显示
+    
+  },
+  onHide:function(){
+    // 页面隐藏
+    
+  },
+  onUnload:function(){
+    // 页面关闭
+    
+  }
+
+
+})
